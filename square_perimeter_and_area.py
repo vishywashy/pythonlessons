34bfa8677d486fe3b5025cf443876ef346a22863
vishywashy <vishrutgiridhar@gmail.com>
2023-02-15 16:33:37 +0000

    Changed a bit of code for area_of_triangle and square_perimeter_and_area.

--- square_perimeter_and_area.py
+++ square_perimeter_and_area.py
@@ -1,19 +1,16 @@
 #This code will ask for the first side and the second side.Based on the numbers you typed it will calculate the perimeter and area.
 def side():
-    global side1, side2
-    side1 = input("Give me the first side:")
-    side2 = input("Give me the second side:")
-
-
-def area_and_perimeter():
-    perimeter = int(side1) + int(side1) + int(side2) + int(side2)
-    area = int(side1) * int(side2)
+    side1 = input("Give me the first side of the square:")
+    return side1
+def area_and_perimeter(side):
+    perimeter = int(side) * 4
+    area = int(side) ** 2
     print("the perimeter is " + str(perimeter))
 
     print("the area is " + str(area))
 
 try:
-  side()
-  area_and_perimeter()
+  calc=side()
+  area_and_perimeter(calc)
 except:
-    print("You need to enter a number")
+    print("You need to enter a number.")
